8eee433ab4d82d6217267a77944e23b46f9c6efe
Luiz Marcelo <marcardion@gmail.com>
2017-02-17 20:26:59 -0400

    Criando o DoorController.cs

---
+++ Assets/DoorController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : InteractableObject {
+
+	public override void Interact ()
+	{
+		gameObject.SetActive (false);
+	}
+}
